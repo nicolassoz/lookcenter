2f49d41774828b05766a427042c1c7ef029a44f6
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-06-12 17:20:29 -0300

    mundança de cor do p da duvida

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
             <h3 style="color: var(--primary-gold)">LookCenter</h3>
             <p class="footer-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
             <p class="footer-text">alguma duvida? entre em contato com a gente através do nosso E-mail ou redes socias</p>
-            <p class="footer-text">LookCenter@gmail.com</p>
+            <p class="footer-text" style="color: var(--primary-gold)">LookCenter@gmail.com</p>
             <div class="py-3">
                 <span class="text-warning mx-2">Instagram</span>
                 <span class="text-warning mx-2">Facebook</span>

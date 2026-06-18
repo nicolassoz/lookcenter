20c61ba8174003c1556fc6be4d3f0830a491d371
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-06-18 14:15:28 -0300

    comentario da descrição (footer)

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,19 @@
+
+<!-- footer descrição do site -->
+
 <footer class="text-center text-lg-start">
         <div class="container text-center">
             <h3 style="color: var(--primary-gold)">LookCenter</h3>
             <p class="footer-text mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
             <p class="footer-text">alguma duvida? entre em contato com a gente através do nosso E-mail ou redes socias</p>
             <p class="footer-text" style="color: var(--primary-gold)">LookCenter@gmail.com</p>
+            <!-- rede socias da lookcenter -->
             <div class="py-3">
                 <span class="text-warning mx-2">Instagram</span>
                 <span class="text-warning mx-2">Facebook</span>
                 <span class="text-warning mx-2">WhatsApp</span>
             </div>
+            <!-- desenvolverdor do site -->
             <p class="footer-text mb-0">&copy; 2026 NextCode - Todos os direitos reservados.</p>
         </div>
     </footer>

a1e4789066a0b60020abc484f4dc77c021e7e970
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-06-18 14:13:21 -0300

    comentarios do navbar menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,14 @@
+
+<!-- o navbar do menu -->
+
 <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container">
         <a class="navbar-brand fs-2 fw-bold" href="index.php">LOOKCENTER</a>
         <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
+
+        <!-- links que levara as outras paginas -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>

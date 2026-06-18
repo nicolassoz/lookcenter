fb6a663f439621f53c10844c65e6da63495683a5
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-06-18 13:45:32 -0300

    comentarios sobre a pagina (sobre nos) ok

--- sobreNos.php
+++ sobreNos.php
@@ -1,3 +1,6 @@
+
+<!-- pagina que fala sobre a lookcenter -->
+
 <?php 
 // adicionar a parte de cima do site
 include "includes/menu.php";
@@ -22,6 +25,8 @@ include "includes/menu.php";
             <h2 class="display-1,">SOBRE NÒS</h2>
             <p class="lead mb-4 text-light">Nós somos a <b>LOKCENTER</b>, uma marca vibrantes que pulsa no ritmo das ruas. Nosso DNA é 100% streetwer criado por e para jovens que não têm medo de ousar e expressar sua identidade única. Acreditamos que a moda é uma ferramenta poderosa de autoexpressão, e por isso, selecionamos curadoria de peças que unem o estilo urbano, o conforto e a atitude.</p>
             <p class="lead mb-4 text-light">Desde os moletons oversize aos acessórios com design exclusivo, cada item na <b>LOOKCENTER</b> conta uma <b>historia de liberdade, criatividade e autenticidade</b>. Mais que uma loja, somos uma <b>comunidade</b> que celebra a cultura jovem e a energia das cidades. vem fazer parte!</p>
+
+            <!-- botão que leva a pagina de roupa -->
             <a href="colecao.php" class="btn btn-gold">VER NOSSA COLEÇÃO</a>
         </div>
     </section>

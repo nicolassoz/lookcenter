8a32e1f2df73da09564fe427c6ad27214c0c2175
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-06-18 14:10:03 -0300

    comentarios offcanvas carrinho ok

--- includes/offcar.php
+++ includes/offcar.php
@@ -1,8 +1,10 @@
 
+<!-- offcanvas que aparecerar o carrinho -->
 
     <div class="justify-content-Center align-items-center offcanvas offcanvas-end bg-black text-white border-end border-secondary" tabindex="-1" id="offcar" aria-labelledby="sidebarFiltrosLabel">
                 <h5 class="offcanvas-title text-gold">Carrinho Vazio</h5>
     </div>
 
+    <!-- se não tiver nada no carrinho tera que aparecer (Carrinho Vazio) -->
 
-
+    <!-- tera que aparecer automaticamente as roupas que o usuario colocou no carrinho-->

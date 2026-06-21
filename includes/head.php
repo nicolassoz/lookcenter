8e38456e4a3d805d0e0ccd424d4a4d333e0febd5
Nicolas Souza da Silva <nnzllznn12@gmail.com>
2026-06-20 22:02:44 -0300

    a

--- includes/head.php
+++
@@ -1,5 +0,0 @@
-
-    <!-- links para o bootstrap, img e style -->
-     <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
--- index.php
+++ index.php
@@ -14,13 +14,8 @@ include "includes/menu.php";
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
-    <?php //include "includes/head.php";?>
+   <link rel="stylesheet" href="assets/css/style.css">
     
-    <script>
-
-    
-
-    </script>
 
     
     

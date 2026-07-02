7ad5d6357908e11e9e4be1e181778eefcfceeaea
Nicolas souza da silva <nnzllznn12@gmail.com>
2026-07-02 16:24:57 -0300

    add carousel nas calças e dois moletom

--- paginaRoupas/pg_calcas/pg_calca_J_B_Classica.php
+++ paginaRoupas/pg_calcas/pg_calca_J_B_Classica.php
@@ -24,7 +24,24 @@ include "../../includes/menu.php"
 
     <div class="row">
         <div class="card col-md-5 m-3">
-            <img src="../../roupas/calcas/Calça_Jeans_Baggy_Clássica_frente.jpg" alt="Calca">
+
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="../../roupas/calcas/Calça_Jeans_Baggy_Clássica_frente.jpg" class="img-fluid" alt="Calca"></div>
+                    <div class="carousel-item">
+                    <img src="../../roupas/calcas/Calça_Jeans_Baggy_Clássica_costa.jpg"  class="img-fluid" alt="Calca"></div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
         </div>
 
         <div class="col-md-5 m-3">
--- paginaRoupas/pg_moletons/pg_moleton_G_red.php
+++ paginaRoupas/pg_moletons/pg_moleton_G_red.php
@@ -24,7 +24,24 @@ include "../../includes/menu.php"
 
     <div class="row">
         <div class="card col-md-5 m-3">
-            <img src="../../roupas/moletons/Moletom_Red_Graphics_frente.jpg" alt="Moletom">
+
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="../../roupas/moletons/Moletom_Red_Graphics_frente.jpg" class="img-fluid" alt="Moletom"></div>
+                    <div class="carousel-item">
+                    <img src="../../roupas/moletons/Moletom_Red_Graphics_costa.jpg"  class="img-fluid" alt="Moletom"></div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
         </div>
 
         <div class="col-md-5 m-3">
